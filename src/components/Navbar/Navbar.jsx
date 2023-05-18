d8581417717ae3d4219d968cdf5103d712536167
MatiasOrlando <matiassorlando@gmail.com>
2023-05-18 21:38:19 +0200

    update styles navbar and color body

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useContext } from "react";
-import Button from "react-bootstrap/Button";
+import Button from "@mui/material/Button";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -25,7 +25,7 @@ const NavBar = () => {
   };
 
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="black" expand="sm">
       <Container fluid>
         <Nav.Link href="/">
           <img
@@ -36,15 +36,23 @@ const NavBar = () => {
             alt="TMBD logo"
           />
         </Nav.Link>
-        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Toggle
+          aria-controls="navbarScroll"
+          bg="light"
+          sx={{ color: "white" }}
+        />
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
+            style={{ maxHeight: "100px", color: "white" }}
             navbarScroll
           >
-            <Nav.Link href="/movies">Movies</Nav.Link>
-            <Nav.Link href="/tvshows">TV Shows</Nav.Link>
+            <Nav.Link href="/movies" style={{ color: "white" }}>
+              Movies
+            </Nav.Link>
+            <Nav.Link href="/tvshows" style={{ color: "white" }}>
+              TV Shows
+            </Nav.Link>
             {userLogged.data && <Nav.Link href="/profile">Profile</Nav.Link>}
           </Nav>
           <div
@@ -56,9 +64,7 @@ const NavBar = () => {
           >
             {userLogged.data ? (
               <Link>
-                <Button className="btn btn-dark" onClick={() => logOut()}>
-                  Log out
-                </Button>
+                <Button onClick={() => logOut()}>Log out</Button>
               </Link>
             ) : (
               <Link to="/loguser">
